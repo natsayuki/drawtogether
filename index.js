3cc318d4c49415a632080c1cba1bd551accbfb9c
the42ndturtle <the42ndturtle@gmail.com>
2018-03-06 17:27:43 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+function beamit(socket, back, message){
+  socket.emit(back, message);
+  socket.broadcast.emit(back, message);
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
+});
+
+app.use(express.static("static"));
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket)=>{
+  beamit(socket, 'connection', 'true');
+  socket.on('draw', (data)=>{
+    beamit(socket, 'returnDraw', data);
+  });
+});
+server.listen(port, () => {
+  console.log("server up on port " + port);
+});
---
+++ static/script.js
@@ -0,0 +1,95 @@
+const socket = io();
+let toggle = 0;
+let drawing = true;
+let shape = 'rect';
+let size = 80;
+
+function setup() {
+  createCanvas(640, 480);
+  frameRate(15);
+  const canvas = $('#defaultCanvas0')
+  canvas.css({'position': 'fixed', 'left': (($(window).width()/2) - (canvas.width()/2)) + 'px',  'border': '.2rem solid grey'});
+}
+function draw(){
+  socket.emit('draw', {'mouseX': mouseX, 'mouseY': mouseY, 'drawing': drawing, 'color': toggle, 'shape': shape, 'size': size});
+  socket.on('returnDraw', (data)=>{
+    mouseX2 = data['mouseX']; mouseY2 = data['mouseY']; drawing = data['drawing']; color = data['color']; shape = data['shape']; size = data['size'];
+    // fill(color)
+    if(drawing){
+      if(shape == 'rect'){
+        rect(mouseX-(size/2), mouseY-(size/2), size, size);
+      }
+      else{
+        ellipse(mouseX2, mouseY2, size, size);
+      }
+    }
+  });
+}
+function keyPressed(){
+  const pen = $('.pen');
+  const colorDiv = $('.color');
+  const shapeDiv = $('.shape');
+  const sizeDiv = $('.size');
+  if(key == ' '){
+    toggle++;
+    toggle %= 5;
+    if(toggle == 0){
+      fill('white');
+      colorDiv.css({'color': 'black'});
+      colorDiv.html('White');
+    }
+    else if(toggle == 1){
+      fill('blue');
+      colorDiv.css({'color': 'blue'});
+      colorDiv.html('Blue');
+    }
+    else if(toggle == 2){
+      fill('red');
+      colorDiv.css({'color': 'red'});
+      colorDiv.html('Red');
+    }
+    else if(toggle == 3){
+      fill('green');
+      colorDiv.css({'color': 'green'});
+      colorDiv.html('Green');
+    }
+    else if(toggle == 4){
+      fill('yellow');
+      colorDiv.css({'color': 'yellow'});
+      colorDiv.html('Yellow');
+    }
+  }
+  else if(keyCode == SHIFT){
+    drawing = !drawing;
+    if(drawing){
+      pen.html('Pen on');
+    }
+    else{
+      pen.html('Pen off');
+    }
+  }
+  else if(keyCode == CONTROL){
+    if(shape == 'rect'){
+      shape = 'ellipse';
+      shapeDiv.html('Circle');
+    }
+    else{
+      shape = 'rect';
+      shapeDiv.html('Square');
+    }
+  }
+  else if(key == 'Q'){
+    size -= 10;
+    if(size<0){
+      size = 0;
+    }
+    sizeDiv.html(size + 'px');
+  }
+  else if(key == 'E'){
+    size += 10;
+    if(size>500){
+      size = 500;
+    }
+    sizeDiv.html(size + 'px');
+  }
+}
